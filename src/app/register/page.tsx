fdf8a1d8e31cfbea69115b2afb57f76bd098012d
Rares Cosa <rares.cosa@gmail.com>
2026-01-29 20:30:12 +0200

    Add Google SingIn, SignOut fix & LoadingState
    - Add Google sign in on register and login page
    - Small improvement in page layout to create more space for the logo
    - Create loading state before user and habits are loaded
    - Fix sign out function by adding async/await. Without this, the Supabase call wasn't finishing before navigation, leaving a token in localStorage that caused register/login pages to redirect back to homepage

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react"
 import Image from "next/image"
 import { supabase } from "@/lib/supabase"
 import { useRouter } from "next/navigation"
+import Link from "next/link"
 
 export default function RegisterPage() {
     const router = useRouter()
@@ -53,9 +54,21 @@ export default function RegisterPage() {
         router.push("/")
     }
 
+    async function handleGoogleRegister() {
+        const {error: singUpGoogleError} = await supabase.auth.signInWithOAuth({
+            provider: 'google'
+        })
+
+        // Handle error
+        if (singUpGoogleError) {
+            setError(singUpGoogleError.message)
+            return
+        }
+    }
+
     return (
         <div className="flex min-h-screen bg-zinc-50 font-sans dark:bg-[#E6E8E6]">
-            <div className="w-1/2 flex flex-col px-35 py-22">
+            <div className="w-1/2 flex flex-col px-35 py-10">
                 <Image
                     src="/easehabit.svg"
                     alt="Easehabit logo"
@@ -96,6 +109,28 @@ export default function RegisterPage() {
                                 Register
                             </button>
                         </form>
+                        <div className="flex flex-col items-center gap-7">
+                            <div className="flex items-center w-full gap-4">
+                                <hr className="flex-1 border-gray-300"/>
+                                <p className="text-sm text-zinc-600">Or Register With</p>
+                                <hr className="flex-1 border-gray-300"/>
+                            </div>
+                            <div className="flex items-center w-full gap-4">
+                                <button onClick={handleGoogleRegister} className="flex flex-1 items-center justify-center px-4 py-2 border gap-2 border-slate-200 dark:border-slate-700 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150">
+                                    <Image src="https://www.svgrepo.com/show/475656/google-color.svg" alt="google logo" width={15} height={15} loading="lazy"/>
+                                    <span>Google</span>
+                                </button>
+        
+                                <button type="button" className="flex flex-1 items-center justify-center px-4 py-2 border gap-2 border-slate-200 dark:border-slate-700 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150">
+                                    <svg className="w-5 h-5" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="apple" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="currentColor" d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z"></path></svg>
+                                    <span>Apple</span>
+                                </button>
+                            </div>
+                            <div className="flex items-center gap-1">
+                                <p className="text-sm text-zinc-600">Already Have An Account?</p>
+                                <Link href="/login" className="text-sm text-[#FF6B6B]">Sing in.</Link>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
